71f8da3dc3428ab119486a24c89fde989907ac8d
cesca <francesca.pucci@polofermigiorgi.it>
2023-03-22 11:46:24 +0100

    seleziona la parola che vuoi

--- src/main/java/com/example/parolesuggerite/HelloController.java
+++ src/main/java/com/example/parolesuggerite/HelloController.java
@@ -3,19 +3,33 @@ package com.example.parolesuggerite;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
 public class HelloController {
+<<<<<<< Updated upstream
     Parole parole = new Parole();
+=======
+    /**attributi*/
+    @FXML
+    private Label control, parolaLabel;
+>>>>>>> Stashed changes
     @FXML
     private ListView paroleListView;
 
     @FXML
     private TextField parolaField;
+<<<<<<< Updated upstream
+=======
+    @FXML
+    private Button selezionaParola;
+    private ArrayList<String> daPassareAListView = new ArrayList<>();
+>>>>>>> Stashed changes
     private StringProperty parola = new SimpleStringProperty();
 
     public HelloController() throws IOException {
@@ -43,6 +57,39 @@ public class HelloController {
         }
     }**/
 
+<<<<<<< Updated upstream
+=======
+    }
+
+    /**
+     * aggiorna gui lista
+     */
+    @FXML
+    protected void aggiornaLista(){
+        paroleListView.getItems().clear();
+        for(int i=0; i<daPassareAListView.size(); i++){
+            paroleListView.getItems().add(daPassareAListView.get(i));
+        }
+    }
+    /**
+     * seleziona la lista
+     */
+    public String getParolaListView(){
+        String s = (String) paroleListView.getFocusModel().getFocusedItem();
+        System.out.println(s);
+        return s;
+    }
+
+    /**
+     * seleziona un elemnto della list view
+     */
+    @FXML
+    public void scegliParola(){
+        parolaLabel.setText(getParolaListView());
+    }
+
+    /**getter/setter*/
+>>>>>>> Stashed changes
     public ListView getParoleListView() {
         return paroleListView;
     }
